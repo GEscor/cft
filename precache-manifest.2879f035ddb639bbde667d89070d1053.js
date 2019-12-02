b7001f8a1bbde694b6899f4627f52ea74d1da4b4
GEscor <escor@inbox.ru>
2019-12-02 17:50:08 +0700

    Updates

---
+++ precache-manifest.2879f035ddb639bbde667d89070d1053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba6324113413c9e83ee896447140384",
+    "url": "/cft/index.html"
+  },
+  {
+    "revision": "c74c780d5e0541c1df0b",
+    "url": "/cft/static/css/main.e794b8ea.chunk.css"
+  },
+  {
+    "revision": "bd59d95afa4e869dde0f",
+    "url": "/cft/static/js/2.a6e7d3ca.chunk.js"
+  },
+  {
+    "revision": "c74c780d5e0541c1df0b",
+    "url": "/cft/static/js/main.4e228903.chunk.js"
+  },
+  {
+    "revision": "1f54e109d0ba4591f8c0",
+    "url": "/cft/static/js/runtime-main.4063f45b.js"
+  }
+]);
